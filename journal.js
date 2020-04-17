b75ce3011c6075f8f03f86b128c4ead2fd05edf8
Stephen Avila <stephen.r.avila@gmail.com>
2020-04-17 15:40:34 -0400

    finish

--- journal.js
+++ journal.js
@@ -1,50 +1,69 @@
 const journalEntry = {
-    dateofEntry: "4/17/2020",
-    conceptsCovered: "Working with DOMs",
-    entryText: "Finishing up practice DOMs",
-    dailyMood:"Happy"
+    Entry: "4/17/2020",
+    Covered: "Working with DOMs",
+    Text: "Finishing up practice DOMs",
+    Mood:"Happy"
 
 };
 
 const journalEntry2 = {
-    dateofEntry: "4/16/2020",
-    conceptsCovered: "Working with functions",
-    entryText: "Finishing up practice functions",
-    dailyMood:"Mad"
+    Entry: "4/16/2020",
+    Covered: "Working with functions",
+    Text: "Finishing up practice functions",
+    Mood:"Mad"
 
 };
 
 const journalEntry3 = {
-    dateofEntry: "4/15/2020",
-    conceptsCovered: "Working with Java/Journal",
-    entryText: "Finishing up practice Java/Journal",
-    dailyMood:"Tired"
+    Entry: "4/15/2020",
+    Covered: "Working with Java/Journal",
+    Text: "Finishing up practice Java/Journal",
+    Mood:"Tired"
 
 };
 
+const Diary = []
+
+Diary.push(journalEntry)
+Diary.push(journalEntry2)
+Diary.push(journalEntry3)
+
+console.log(Diary)
+
 const makeJournalEntryComponent = (journalEntry)=>{
        return `
-    <form class="flex-container">
-    <label>${journalEntry.dateofEntry}</label>
-    <label>${journalEntry.conceptsCovered}</label>
-    <label>${journalEntry.entryText}</label>
-    <label>${journalEntry.dailyMood}</label>
-    </form>
+    <section class="flex-container">
+        <h1>${journalEntry.Entry}</h1>
+        <br>
+        <h3>${journalEntry.Covered}</h3>
+        <p>${journalEntry.Text}</p>
+        <h4>${journalEntry.Mood}</h4>
+    </section>
     `
 }
-const htmlString = makeJournalEntryComponent(journalEntry)
-console.log(htmlString)
 
+// const htmlString = makeJournalEntryComponent(journalEntry)
+// console.log(htmlString)
 
-const journalEntriesArray = [journalEntry,journalEntry2,journalEntry3];
+// for(singleEntry of Diary){
+//     let domEntry = makeJournalEntryComponent(singleEntry)
 
-// journalEntriesArray.push(journalEntry,journalEntry2,journalEntry3)
-
-console.log(journalEntriesArray)
-
-
-// const renderJournalEntries = (journalEntriesArray) => {
-
+//     document.querySelector(".entryLog").innerHTML += domEntry
 // }
 
-// renderJournalEntries(journalEntriesArray)
+
+
+
+
+
+
+
+const renderJournalEntries = (entries) => {
+    for(singleEntry of entries){
+        let domEntry = makeJournalEntryComponent(singleEntry)
+    
+        document.querySelector(".entryLog").innerHTML += domEntry
+    }
+}
+
+renderJournalEntries(Diary)

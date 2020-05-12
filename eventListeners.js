ee926fb0aa1c73bbe043cba4eeee2647e842cdfa
StephenAvila2020 <stephen.r.avila@gmail.com>
2020-05-12 15:19:25 -0400

    added

--- eventListeners.js
+++ eventListeners.js
@@ -1,28 +1,6 @@
 
-import apiManager from "./apiManager.js";
-import renderJournalEntries from "./domPrinter.js";
-
-const entryEvent = () => {
-    const entryDate = document.querySelector("#journalDate").value;
-    const entryConcept = document.querySelector("#journalConcept").value;
-    const entryEntry = document.querySelector("#journalEntry").value;
-    const entryMood = document.querySelector("#mood").value;
 
 
-    const journalEntryObject = {
-        Date: entryDate,
-        Concept: entryConcept,
-        Entry: entryEntry,
-        Mood: entryMood,
-    }
 
+  
 
-    document.querySelector("#journalContainer").innerHTML = "";
-
-    apiManager.getAllJournalsFromAPI().then((entries) => {
-        renderJournalEntries(entries)
-    })
-
-}
-
-export default entryEvent
--- journal.js
+++ journal.js
@@ -1,17 +1,36 @@
 import apiManager from "./apiManager.js"
 import renderJournalEntries from "./domPrinter.js"
-import entryEvent from "./eventListeners.js"
 
-//Print all of the Journal entries
-apiManager.getAllJournalsFromAPI().then((entries) => {
-    console.log(entries)
-    renderJournalEntries(entries)
-})
+
 
 // Add a click event listener to the Journal Entry button
 
-document.querySelector("#journalBtn").addEventListener("click", entryEvent)
-        console.log(entryEvent)
+document.querySelector("#journalBtn").addEventListener("click", function () {
+
+    const entryDate = document.querySelector("#journalDate").value;
+    const entryConcept = document.querySelector("#journalConcept").value;
+    const entryEntry = document.querySelector("#journalEntry").value;
+    const entryMood = document.querySelector("#mood").value;
+
+        console.log(entryConcept,entryDate,entryMood,entryEntry)
+    const journalEntryObject = {
+        Date: entryDate,
+        Concept: entryConcept,
+        Entry: entryEntry,
+        Mood: entryMood,
+    };
+
+    
+
+    document.querySelector("#journalContainer").innerHTML = "";
+
+    apiManager.getAllJournalsFromAPI().then((entries) => {
+        renderJournalEntries(entries)
+    })
+})
+
+
+        
 
     
     
